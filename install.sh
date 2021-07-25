688b41ada16102e50e97b541a24568f488bb5cd3
Oscar Holm <oscar@numbery.se>
2021-07-25 11:26:05 +0200

    Adding installation script

---
+++ install.sh
@@ -0,0 +1,4 @@
+## Creating symbolic links to dotfiles folder which should be cloned into $HOME
+ln -s ~/dotfiles/.bash/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.bash/.bash_profile ~/.bash_profile
+ln -s ~/dotfiles/git/.gitconfig ~/.gitconfig
